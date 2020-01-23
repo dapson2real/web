aa59cd307704110a07aec084d1bbd5a24e4be915
Adedapo Awolayo <58064667+dapson2real@users.noreply.github.com>
2020-01-23 00:33:27 -0600

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
+    "Berkeley, California | UC-Berkeley: Human Contexts and Ethics of Data course | ",
     37.8708393,
-    -122.2728638
+    -122.2728639
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Long Beach, CA, USA | Digital Media and Learning (DML) | ",
+    33.78538945,
+    -118.158049315311
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Austin, Texas | Conference on Human Factors in Computing (CHI) | ",
+    30.2711286,
+    -97.7436995
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Paris, France | OpenSym | ",
+    48.826543, 
+    2.346576
   ]
 ];
